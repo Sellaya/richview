2fe9d86c69c35c6e99ddb75b46e88797e703eba7
Sellaya <sellayadigital@gmail.com>
2026-03-26 19:34:48 -0400

    Implement professional design system

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,21 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/images/[name]-[hash][extname]`;
+          } else if (/css/i.test(ext)) {
+            return `assets/css/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        }
+      }
+    }
   }
 });
